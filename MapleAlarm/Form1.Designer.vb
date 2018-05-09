a9f47f6d72a5d4d905da23e78bd00071b17848e5
Elbitty <conor.dev@gmail.com>
2018-05-09 21:53:01 +0900

    First Commit
    Just upload

---
+++ MapleAlarm/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.PinkThe = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox9 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox10 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox11 = New System.Windows.Forms.CheckBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Everyday = New System.Windows.Forms.Timer(Me.components)
+        Me.Thursday = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox12 = New System.Windows.Forms.CheckBox()
+        Me.Museum = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(13, 14)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(229, 24)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "정시 15분 핑크빈 씨어터 알림"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Enabled = False
+        Me.CheckBox2.Location = New System.Drawing.Point(13, 108)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(93, 24)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "홀리 심볼"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(110, 105)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(39, 27)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "10"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Enabled = False
+        Me.Label1.Location = New System.Drawing.Point(155, 109)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "분마다 알림"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Enabled = False
+        Me.CheckBox3.Location = New System.Drawing.Point(13, 164)
+        Me.CheckBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(194, 24)
+        Me.CheckBox3.TabIndex = 4
+        Me.CheckBox3.Text = "12시 플래그 레이스 알림"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Enabled = False
+        Me.CheckBox4.Location = New System.Drawing.Point(13, 198)
+        Me.CheckBox4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(186, 24)
+        Me.CheckBox4.TabIndex = 5
+        Me.CheckBox4.Text = "7시 플래그 레이스 알림"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Enabled = False
+        Me.CheckBox5.Location = New System.Drawing.Point(13, 232)
+        Me.CheckBox5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(186, 24)
+        Me.CheckBox5.TabIndex = 6
+        Me.CheckBox5.Text = "9시 플래그 레이스 알림"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Location = New System.Drawing.Point(13, 288)
+        Me.CheckBox7.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(121, 24)
+        Me.CheckBox7.TabIndex = 8
+        Me.CheckBox7.Text = "매일 0시 알림"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Enabled = False
+        Me.CheckBox8.Location = New System.Drawing.Point(13, 322)
+        Me.CheckBox8.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(136, 24)
+        Me.CheckBox8.TabIndex = 10
+        Me.CheckBox8.Text = "목요일 0시 알림"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'PinkThe
+        '
+        Me.PinkThe.Interval = 1000
+        '
+        'CheckBox9
+        '
+        Me.CheckBox9.AutoSize = True
+        Me.CheckBox9.Enabled = False
+        Me.CheckBox9.Location = New System.Drawing.Point(13, 377)
+        Me.CheckBox9.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox9.Name = "CheckBox9"
+        Me.CheckBox9.Size = New System.Drawing.Size(367, 24)
+        Me.CheckBox9.TabIndex = 12
+        Me.CheckBox9.Text = "보스1 입장 대기 시간 30분 (자동으로 체크 해제됨)"
+        Me.CheckBox9.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Enabled = False
+        Me.CheckBox6.Location = New System.Drawing.Point(13, 411)
+        Me.CheckBox6.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(367, 24)
+        Me.CheckBox6.TabIndex = 13
+        Me.CheckBox6.Text = "보스2 입장 대기 시간 30분 (자동으로 체크 해제됨)"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox10
+        '
+        Me.CheckBox10.AutoSize = True
+        Me.CheckBox10.Enabled = False
+        Me.CheckBox10.Location = New System.Drawing.Point(13, 445)
+        Me.CheckBox10.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox10.Name = "CheckBox10"
+        Me.CheckBox10.Size = New System.Drawing.Size(367, 24)
+        Me.CheckBox10.TabIndex = 14
+        Me.CheckBox10.Text = "보스3 입장 대기 시간 30분 (자동으로 체크 해제됨)"
+        Me.CheckBox10.UseVisualStyleBackColor = True
+        '
+        'CheckBox11
+        '
+        Me.CheckBox11.AutoSize = True
+        Me.CheckBox11.Enabled = False
+        Me.CheckBox11.Location = New System.Drawing.Point(13, 479)
+        Me.CheckBox11.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox11.Name = "CheckBox11"
+        Me.CheckBox11.Size = New System.Drawing.Size(367, 24)
+        Me.CheckBox11.TabIndex = 15
+        Me.CheckBox11.Text = "보스4 입장 대기 시간 30분 (자동으로 체크 해제됨)"
+        Me.CheckBox11.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "메이플 알리미가 작동 중 입니다."
+        Me.NotifyIcon1.BalloonTipTitle = "알림"
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "메이플 알리미"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(70, 514)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(129, 20)
+        Me.LinkLabel1.TabIndex = 17
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "@gloomy_connor"
+        '
+        'Everyday
+        '
+        Me.Everyday.Interval = 1000
+        '
+        'Thursday
+        '
+        Me.Thursday.Interval = 1000
+        '
+        'CheckBox12
+        '
+        Me.CheckBox12.AutoSize = True
+        Me.CheckBox12.Location = New System.Drawing.Point(13, 48)
+        Me.CheckBox12.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.CheckBox12.Name = "CheckBox12"
+        Me.CheckBox12.Size = New System.Drawing.Size(365, 24)
+        Me.CheckBox12.TabIndex = 18
+        Me.CheckBox12.Text = "ARK 뮤지엄 성향 500 알림 (자동으로 체크 해제됨)"
+        Me.CheckBox12.UseVisualStyleBackColor = True
+        '
+        'Museum
+        '
+        Me.Museum.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(385, 556)
+        Me.Controls.Add(Me.CheckBox12)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.CheckBox11)
+        Me.Controls.Add(Me.CheckBox10)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox9)
+        Me.Controls.Add(Me.CheckBox8)
+        Me.Controls.Add(Me.CheckBox7)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "메이플 알리미"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents PinkThe As Timer
+    Friend WithEvents CheckBox9 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents CheckBox11 As CheckBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Everyday As Timer
+    Friend WithEvents Thursday As Timer
+    Friend WithEvents CheckBox12 As CheckBox
+    Friend WithEvents Museum As Timer
+End Class
---
+++ MapleAlarm/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+    Dim MuseumTimeMin As Integer
+    Dim MuseumTimeSec As Integer
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub ToTray()
+        Me.Hide()
+        NotifyIcon1.Visible = True
+        NotifyIcon1.ShowBalloonTip(200)
+    End Sub
+
+
+    Private Sub notifyIcon1_MouseDoubleClick(ByVal sender As Object, ByVal e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Visible = True
+        If Me.WindowState = FormWindowState.Minimized Then Me.WindowState = FormWindowState.Normal
+        Me.Activate()
+        Me.NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        If FormWindowState.Minimized = WindowState Then ToTray()
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://twitter.com/gloomy_connor")
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            PinkThe.Enabled = True
+        Else
+            PinkThe.Enabled = False
+        End If
+    End Sub
+
+    Private Sub PinkThe_Tick(sender As Object, e As EventArgs) Handles PinkThe.Tick
+
+        If Mid(TimeString, 4, 5) = "14:15" Then
+            NotifyIcon1.ShowBalloonTip(200, "핑크빈 씨어터", "곧 정시 15분 입니다. 핑크빈 씨어터에 방문하세요.", ToolTipIcon.Info)
+            My.Computer.Audio.Play(My.Resources.Resource1.up, AudioPlayMode.Background)
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CheckBox7_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox7.CheckedChanged
+        If CheckBox7.Checked = True Then
+            Everyday.Enabled = True
+        Else
+            Everyday.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Everyday_Tick(sender As Object, e As EventArgs) Handles Everyday.Tick
+
+        If TimeString = "23:59:05" Then
+            NotifyIcon1.ShowBalloonTip(200, "날짜 변경", "곧 0시입니다. 마일리지를 정산 하세요. 데일리 기프트를 수령하세요.", ToolTipIcon.Info)
+            My.Computer.Audio.Play(My.Resources.Resource1.trans, AudioPlayMode.Background)
+        End If
+
+    End Sub
+
+    Private Sub CheckBox12_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox12.CheckedChanged
+        If CheckBox12.Checked = True Then
+            Museum.Enabled = True
+            MuseumTimeMin = Val(Mid(TimeString, 4, 2))
+            MuseumTimeSec = Val(Mid(TimeString, 7, 2))
+
+            Debug.Print(MuseumTimeSec)
+
+            MuseumTimeMin = MuseumTimeMin + 42
+            Debug.Print(MuseumTimeMin)
+
+            If MuseumTimeMin >= 60 Then MuseumTimeMin = MuseumTimeMin - 60
+        Else
+            Museum.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Museum_Tick(sender As Object, e As EventArgs) Handles Museum.Tick
+
+        Dim MuseumTime As String = MuseumTimeMin.ToString & ":" & MuseumTimeSec.ToString
+
+        If Mid(TimeString, 4, 5) = MuseumTime Then
+            NotifyIcon1.ShowBalloonTip(200, "ARK 뮤지엄", "성향 500을 모두 달성했습니다.", ToolTipIcon.Info)
+            My.Computer.Audio.Play(My.Resources.Resource1.up, AudioPlayMode.Background)
+            CheckBox12.Checked = False
+            Museum.Enabled = False
+        End If
+
+
+    End Sub
+End Class
